b06b25e30562ed4f4d4caf9da5a247029f3bdb3e
= <samueltennant@hotmail.com>
2023-05-28 22:45:27 +0100

    added 16 bit read functions

--- src/cpu.rs
+++ src/cpu.rs
@@ -44,6 +44,20 @@ impl CPU {
         self.memory[addr as usize] = value;
     }
 
+    // following two functions implement little endianness
+    fn mem_read_u16(&mut self, pos: u16) -> u16 {
+        let lo = self.mem_read(pos) as u16;
+        let hi = self.mem_read(pos + 1) as u16;
+        (hi << 8) | (lo as u16)
+    }
+
+    fn mem_write_u16(&mut self, pos: u16, data: u16) {
+        let hi = (data >> 8) as u8;
+        let lo = (data & 0xff) as u8;
+        self.mem_write(pos, lo);
+        self.mem_write(pos + 1, hi);
+    }
+
     pub fn load_and_run(&mut self, program: Vec<u8>) {
         self.load(program);
         self.run();
